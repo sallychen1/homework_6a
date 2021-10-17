189635aae042622458526b43f004341353440a43
Sally Chen <sally_chen@brown.edu>
2021-10-17 09:23:21 -0400

    changed cart

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 function changePrice() {
-    var x = document.getElementById("qty-select").value;
+    var x = document.getElementById("qty-select").value * 4.99;
     document.getElementById("price").innerHTML = "$ " + x;
 }
